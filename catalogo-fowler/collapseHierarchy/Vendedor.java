f3ee517a8a225364ac5ea11a75cad89d908eb691
Diego Guedes <diegoamericoguedes@gmail.com>
2019-09-11 11:32:36 -0300

    Update Vendedor.java

--- catalogo-fowler/collapseHierarchy/Vendedor.java
+++ catalogo-fowler/collapseHierarchy/Vendedor.java
@@ -5,6 +5,14 @@ import java.math.BigDecimal;
 public class Vendedor extends Empregado{
 	private BigDecimal comissao;
 	
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+
 	public BigDecimal calcularSalario() {		
 		return this.getSalario().add(comissao);
 		
